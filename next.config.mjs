b51f6964f9cbb93a7f846ecd4192009787039335
AnshLokhande <priyanshulokhande72@gmail.com>
2024-10-27 22:10:25 +0530

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-    eslint :{
-        ignoreDuringBuilds: true,
-    }
-    
+    images: {
+        unoptimized: true,
+    },
     
 };
 
- module.exports  = nextConfig;
+export default nextConfig;
